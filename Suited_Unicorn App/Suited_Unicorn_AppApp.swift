49be56cc76097ab9ca64db8b402c25d4e029bf6e
Divak K Maheshwari <divak.maheshwari@icloud.com>
2023-06-24 22:01:53 -0500

    Initial Commit

---
+++ Suited_Unicorn App/Suited_Unicorn_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suited_Unicorn_AppApp.swift
+//  Suited_Unicorn App
+//
+//  Created by Divak Maheshwari on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Suited_Unicorn_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
