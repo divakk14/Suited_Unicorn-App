754970b559108dfece11dea82b9846a77cb25ed6
Haris Adnan <haris.adnan@duke.edu>
2023-07-12 01:38:51 -0500

    completed SidemenuViewModel startigng

---
+++ Suited_Unicorn App/Core/Profile/Views/SideMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  SideMenuView.swift
+//  Suited_Unicorn App
+//
+//  Created by Haris Adnan on 7/12/23.
+//
+
+import SwiftUI
+
+struct SideMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SideMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        SideMenuView()
+    }
+}
