5ccfd52e2fd847f2887f59c59b8ba823e6aecf05
Haris Adnan <haris.adnan@duke.edu>
2023-07-13 05:07:22 -0500

    completed button functionality on bottom right corner of FeedView

--- Suited_Unicorn App/Core/Feed/View/FeedView.swift
+++ Suited_Unicorn App/Core/Feed/View/FeedView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct FeedView: View {
+    @State private var showNewTweetView = false
     var body: some View {
         
-        ScrollView{
-            LazyVStack{
-                ForEach(0 ... 20, id: \.self){ _ in
-                    TweetsRowView()
-                        .padding()
+        
+        ZStack (alignment: .bottomTrailing){
+            ScrollView{
+                LazyVStack{
+                    ForEach(0 ... 20, id: \.self){ _ in
+                        TweetsRowView()
+                            .padding()
+                        
+                    }
                     
                 }
-                
             }
+            Button{
+                showNewTweetView.toggle()
+            } label : {
+                Image(systemName: "plus")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 20, height: 20)
+                    .padding()
+            }
+            .background(Color(.systemBlue)).foregroundColor(.white)
+            .clipShape(Circle())
+            .padding()
+            .fullScreenCover(isPresented: $showNewTweetView){
+                Text("New tweet veiew")
+            }
+            
+            
         }
     }
     
