164b1afd49793dd791a9d0d4720c767b9959b1d6
Divak K Maheshwari <divak.maheshwari@icloud.com>
2023-07-24 15:41:35 -0500

    UI is fully setup with all the requirements. Video timestamp (02:24). Now jumping to backend (firebase).

--- Suited_Unicorn App/Core/Authentication/Views/RegistrationView.swift
+++ Suited_Unicorn App/Core/Authentication/Views/RegistrationView.swift
@@ -9,30 +9,90 @@ import SwiftUI
 
 struct RegistrationView: View {
     @State private var email = ""
+    @State private var age = ""
     @State private var username = ""
     @State private var fullname = ""
     @State private var password = ""
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
         VStack{
             AuthHeaderView(title1: "Get Started", title2: "Create Your Account.")
             
-            VStack{
+            VStack(spacing: 40){
+//                Text("Registeration")
+//                    .font(.largeTitle)
+//                    .padding(.bottom, 30)
+//                HStack{
+//                    Text("Full name:")
+//                    TextField("Enter your fullname", text:$fullname)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                }
+//                .padding(.bottom, 20)
+//
+//                HStack{
+//                    Text("Age:")
+//                    TextField("Enter your age", text:$age)
+//                        .keyboardType(.numberPad)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                }
+//                .padding(.bottom, 20)
+//
+//                HStack{
+//                    Text("Age:")
+//                    TextField("Enter your age", text:$age)
+//                        .keyboardType(.numberPad)
+//                        .textFieldStyle(RoundedBorderTextFieldStyle())
+//                }
+//                .padding(.bottom, 20)
+//
+                
+                
+                
+                CustominputField(imageName: "person",
+                                 placeholderText: "Fullname",
+                                 text: $fullname)
+                CustominputField(imageName: "person",
+                                 placeholderText: "Username",
+                                 text: $username)
                 CustominputField(imageName: "envelope",
                                  placeholderText: "Email",
-                                 text: $password)
-                CustominputField(imageName: "envelope",
-                                 placeholderText: "Email",
-                                 text: $password)
-                CustominputField(imageName: "envelope",
-                                 placeholderText: "Email",
-                                 text: $password)
-                CustominputField(imageName: "envelope",
-                                 placeholderText: "Email",
+                                 text: $email)
+                CustominputField(imageName: "lock",
+                                 placeholderText: "Password",
                                  text: $password)
             }
+            .padding(32)
+            
+            Button{
+                print("Sign up here..")
+            }label:{
+                Text("Sign Up")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(width: 340, height: 50)
+                    .background(Color(.systemBlue))
+                    .clipShape(Capsule())
+                    .padding()
+            }
+            .shadow(color: .gray.opacity(0.5),radius: 10, x:0, y:0)
             
             Spacer()
+            
+            Button{
+                presentationMode.wrappedValue.dismiss()
+                
+            }label: {
+                HStack{
+                    Text("Already have an account?")
+                        .font(.footnote)
+                    Text("Sign In")
+                        .font(.body)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding(.bottom, 32)
+//            .foregroundColor(Color(.systemBlue))
         }
         .ignoresSafeArea()
     }
