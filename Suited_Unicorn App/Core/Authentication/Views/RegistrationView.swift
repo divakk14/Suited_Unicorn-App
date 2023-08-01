3ff8086a76220ebdbd8610baa6e232a001175784
Haris Adnan <haris.adnan@duke.edu>
2023-07-31 21:37:17 -0500

    code now works 3:23

--- Suited_Unicorn App/Core/Authentication/Views/RegistrationView.swift
+++ Suited_Unicorn App/Core/Authentication/Views/RegistrationView.swift
@@ -1,10 +1,3 @@
-//
-//  RegistrationView.swift
-//  Suited_Unicorn App
-//
-//  Created by Divak Maheshwari on 7/17/23.
-//
-
 import SwiftUI
 
 struct RegistrationView: View {
@@ -15,72 +8,71 @@ struct RegistrationView: View {
     @State private var password = ""
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var viewModel: AuthViewModel
-    
-    var body: some View {
-        VStack{
-            NavigationLink(destination: ProfilePhotoSelectorView(),
-                           isActive: $viewModel.didAuthenticateUser,
-                           label: {})
-            
-            AuthHeaderView(title1: "Get Started", title2: "Create Your Account.")
-            
-            VStack(spacing: 40){
-                CustominputField(imageName: "person",
-                                 placeholderText: "Fullname",
-                                 text: $fullname)
-                CustominputField(imageName: "person",
-                                 placeholderText: "Username",
-                                 text: $username)
-                CustominputField(imageName: "person",
-                                 placeholderText: "age",
-                                 text: $age)
-                CustominputField(imageName: "envelope",
-                                 placeholderText: "Email",
-                                 text: $email)
-                CustominputField(imageName: "lock",
-                                 placeholderText: "Password",
-                                 isSecureField: true,
-                                 text: $password)
-            }
-            .padding(32)
-            
-            Button{
-                viewModel.register(withEmail: email,
-                                   password: password,
-                                   fullname: fullname,
-//                                   age: age,
-                                   username: fullname)
 
-            }label:{
-                Text("Sign Up")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .frame(width: 340, height: 50)
-                    .background(Color(.systemBlue))
-                    .clipShape(Capsule())
-                    .padding()
-            }
-            .shadow(color: .gray.opacity(0.5),radius: 10, x:0, y:0)
-            
-            Spacer()
-            
-            Button{
-                
-                presentationMode.wrappedValue.dismiss()
-                
-            }label: {
-                HStack{
-                    Text("Already have an account?")
-                        .font(.footnote)
-                    Text("Sign In")
-                        .font(.body)
-                        .fontWeight(.semibold)
+    var body: some View {
+        NavigationView {
+            VStack {
+                if viewModel.didAuthenticateUser {
+                    ProfilePhotoSelectorView()
+                } else {
+                    VStack {
+                        AuthHeaderView(title1: "Get Started", title2: "Create Your Account.")
+                        
+                        VStack(spacing: 40) {
+                            CustominputField(imageName: "person",
+                                             placeholderText: "Fullname",
+                                             text: $fullname)
+                            CustominputField(imageName: "person",
+                                             placeholderText: "Username",
+                                             text: $username)
+                            CustominputField(imageName: "person",
+                                             placeholderText: "age",
+                                             text: $age)
+                            CustominputField(imageName: "envelope",
+                                             placeholderText: "Email",
+                                             text: $email)
+                            CustominputField(imageName: "lock",
+                                             placeholderText: "Password",
+                                             isSecureField: true,
+                                             text: $password)
+                        }
+                        .padding(32)
+                        
+                        Button {
+                            viewModel.register(withEmail: email,
+                                               password: password,
+                                               fullname: fullname,
+                                               username: username)
+                        } label: {
+                            Text("Sign Up")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(width: 340, height: 50)
+                                .background(Color(.systemBlue))
+                                .clipShape(Capsule())
+                                .padding()
+                        }
+                        .shadow(color: .gray.opacity(0.5),radius: 10, x:0, y:0)
+                        
+                        Spacer()
+                        
+                        Button {
+                            presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            HStack {
+                                Text("Already have an account?")
+                                    .font(.footnote)
+                                Text("Sign In")
+                                    .font(.body)
+                                    .fontWeight(.semibold)
+                            }
+                        }
+                        .padding(.bottom, 32)
+                    }
+                    .ignoresSafeArea()
                 }
             }
-            .padding(.bottom, 32)
-//            .foregroundColor(Color(.systemBlue))
         }
-        .ignoresSafeArea()
     }
 }
 
