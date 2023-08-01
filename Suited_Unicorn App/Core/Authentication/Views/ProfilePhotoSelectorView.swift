c951f8a2686ce77879ef7c7b2eeaa8d1a4d3dccc
Divak K Maheshwari <divak.maheshwari@icloud.com>
2023-08-01 02:54:16 -0500

    Please fix the plus icon photo.

--- Suited_Unicorn App/Core/Authentication/Views/ProfilePhotoSelectorView.swift
+++ Suited_Unicorn App/Core/Authentication/Views/ProfilePhotoSelectorView.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Divak Maheshwari on 7/30/23.
 //
-
 import SwiftUI
-
 struct ProfilePhotoSelectorView: View {
     @State private var showImagePicker = false
     @State private var selectedImage : UIImage?
@@ -15,39 +13,39 @@ struct ProfilePhotoSelectorView: View {
         VStack{
             AuthHeaderView(title1: "Setup account",
                            title2: "Add a profile picture")
-            
             Button{
                 showImagePicker.toggle()
-                
             }label:{
                 if let profileImage = profileImage{
                     profileImage
-                        .resizable()
-                        .renderingMode(.template)
-                        .foregroundColor(Color(.systemBlue))
-                        .scaledToFill()
-                        .frame(width: 100, height: 100)
-                        .padding(.top, 44)
-                        .clipShape(Circle())
-                }else{
+                        .modifier(ProfileImageModifier())
+                } else {
                     Image("photo_plus")
-                        .resizable()
                         .renderingMode(.template)
-                        .foregroundColor(Color(.systemBlue))
-                        .scaledToFill()
-                        .frame(width: 100, height: 100)
-                        .padding(.top, 44)
-                        .clipShape(Circle())
+                        .modifier(ProfileImageModifier())
                 }
-                
             }
             .sheet(isPresented: $showImagePicker){
                 ImagePicker(selectedImage: $selectedImage)
             }
-        
+            .padding(.top, 44)
             Spacer()
         }
         .ignoresSafeArea()
+    }
+    func loadImage (){
+        guard let selectedImage = selectedImage else { return }
+        profileImage = Image(uiImage: selectedImage)
+    }
+}
+
+private struct ProfileImageModifier: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(Color(.systemBlue))
+            .scaledToFill()
+            .frame(width: 100, height: 100)
+            .clipShape(Circle())
     }
 }
 
